e529296c2291251d243434f7744868c3c03a9b98
Yingchao Zou <yczou@icloud.com>
2016-10-02 11:07:24 +0800

    The Basics

---
+++ Introduction_to_Programming_with_Ruby/The_Basics/exercises.rb
@@ -0,0 +1,44 @@
+# Yingchao Zou
+puts "Yingchao" + " " + "Zou"
+
+# thousands place
+puts 1234 / 1000
+
+# hundreds place
+puts 1234 % 1000 / 100
+
+# tens place
+puts 1234 % 1000 % 100 / 10
+
+# ones place
+puts 1234 % 1000 % 100 % 10
+
+# 
+movies = {
+  film1: 2001,
+  film2: 2002,
+  film3: 2003
+}
+
+puts movies[:film3]
+puts movies[:film2]
+puts movies[:film1]
+
+#
+dates = [2001, 2002, 2003]
+
+puts dates[2]
+puts dates[1]
+puts dates[0]
+
+#
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+#
+puts 1.1 * 1.1
+puts 2.2 * 2.2
+puts 3.3 * 3.3
+

953d56536d4df52d53937d95eb454fceee55bc6f
Yingchao Zou <yczou@icloud.com>
2016-10-02 12:50:13 +0800

    Variables

--- Introduction_to_Programming_with_Ruby/Variables/name.rb
+++ Introduction_to_Programming_with_Ruby/Variables/name.rb
@@ -1,3 +1,11 @@
-puts "What is your name"
-name = gets.chomp
+puts "What is your first name"
+first_name = gets.chomp
+puts "What is your last name"
+last_name = gets.chomp
+name = first_name + " " + last_name
+
 puts "Hello" + " " + name
+
+10.times do 
+  puts name
+end

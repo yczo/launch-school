6e2c8e42b8456cec24483afb91e4465c3df930f0
Yingchao Zou <yczou@icloud.com>
2016-10-03 16:14:09 +0800

    Methods

---
+++ Introduction_to_Programming_with_Ruby/Methods/exercise.rb
@@ -0,0 +1,30 @@
+#
+def greeting(name)
+  "Hello" + " " + name
+end
+
+puts greeting("Zou")
+
+#
+x = 1
+puts x = 2
+p name = "Joe"
+four = "four"
+print something = "nothing"
+
+#
+def multiply(number1, number2)
+  number1 * number2
+end
+
+puts multiply(4, 2)
+
+#
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
+
+
